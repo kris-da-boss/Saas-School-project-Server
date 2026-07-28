e0120e9a46993b0eadfa121389730a8adff2526e
kris-da-boss <kriswilson00000@gmail.com>
2026-07-28 22:46:10 +0100

    still testing refresh bug

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { login, refresh, logout, bootstrapSuperAdmin } = require("../controllers/auth.controller");
+const { login, refresh, logout, bootstrapSuperAdmin, getMe } = require("../controllers/auth.controller");
+const protect = require("../middlewares/auth");
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ router.post("/login", login);
 router.post("/refresh", refresh);
 router.post("/logout", logout);
 router.post("/bootstrap-superadmin", bootstrapSuperAdmin);
+router.get("/me", protect, getMe);
 
 module.exports = router;
